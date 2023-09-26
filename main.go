13f77339cf75a184e7527606cc6d1829eaa69df0
tom55dev <thomasbill55dev@gmail.com>
2023-09-27 04:55:56 +1000

    Changed using cors external module and completed

--- main.go
+++ main.go
@@ -1,29 +1,38 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 	"shopping-list-app/graph"
+
+	"github.com/rs/cors"
 )
 
 func main() {
 	shoppingListHandler := func(w http.ResponseWriter, r *http.Request) {
-		// Set CORS headers
-		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-		w.Header().Set("Access-Control-Allow-Credentials", "true") // Set the credentials
+
+		// Log the request
+		log.Printf("Received request: %s %s", r.Method, r.URL.Path)
 
 		// Call actual handler function
 		graph.ShoppingListGraphHandler(w, r)
 	}
 
-	http.HandleFunc("/shoppinglist", shoppingListHandler)
+	mux := http.NewServeMux()
+
+	// Replace this with your actual API setup.
+	mux.HandleFunc("/shoppinglist", shoppingListHandler)
+
+	// Use default options
+	handler := cors.Default().Handler(mux)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	http.ListenAndServe(":"+port, nil)
+	log.Printf("Server listening on port %s", port)
+
+	http.ListenAndServe(":"+port, handler)
 }
